f7e4a43fceeb006ba2ed84f6bf3ac625209cda3f
gopinath patro <goppatro@in.ibm.com>
2020-07-16 22:01:57 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HMCTable from './components/datatables/HMCTable.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<HMCTable/>
+    </div>
+  );
+}
+
+export default App;

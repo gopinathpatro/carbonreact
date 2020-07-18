023980030f445ea31581c54e4bed79a7a49cdd7e
Ravishankar H Ramaswamy <ravrammy@in.ibm.com>
2020-07-18 07:10:27 +0530

    Initial Setup for Springboot backend

---
+++ customer-portal/src/test/java/com/techmount/customerportal/CustomerPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techmount.customerportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

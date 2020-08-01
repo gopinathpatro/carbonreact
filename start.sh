de44b1261d8bd5af2be33a86a230209832e9562a
Ravishankar Ramaswamy <>
2020-08-02 02:22:44 +0530

    Add rabitmq to our docker irfrastructure with data volume folder

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 ./gradlew customer-portal:clean customer-portal:build customer-portal:docker -x test
 docker-compose up -d
+rabbitmq_cont_id=$(docker ps -aqf "name=rabbitmq") && docker exec $rabbitmq_cont_id rabbitmq-plugins enable rabbitmq_management

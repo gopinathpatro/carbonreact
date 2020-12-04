a1da2f4cae483f3f4cd8e6b63729e454f86ee4d4
Ravishankar Ramaswamy <>
2020-12-04 14:36:55 +0530

    Dockerization of all services

--- react/src/App.js
+++ react/src/App.js
@@ -1,8 +1,10 @@
-import React, { Component, Suspense } from 'react';
+import React, { Component, Suspense } from "react";
 // import CustomTable from './components/datatables/CustomTable.js';
-import './App.css';
+import "./App.css";
 
-const CustomTable = React.lazy(() => import('./components/datatables/CustomTable.js'));
+const CustomTable = React.lazy(() =>
+  import("./components/datatables/CustomTable.js")
+);
 
 class App extends Component {
   state = {
@@ -11,39 +13,49 @@ class App extends Component {
     header: [],
     menus: [
       {
-        "name": "SaveAs",
-        "id": "id001"
-      }, {
-        "name": "Copy",
-        "id": "id002"
-      }, {
-        "name": "Delete",
-        "id": "id003"
-      }],
+        name: "SaveAs",
+        id: "id001",
+      },
+      {
+        name: "Copy",
+        id: "id002",
+      },
+      {
+        name: "Delete",
+        id: "id003",
+      },
+    ],
   };
   componentDidMount = () => {
-    fetch('http://localhost:8080/customer/all')
-      .then(res => res.json())
+    fetch("http://localhost:9091/customer/all")
+      .then((res) => res.json())
       .then((data) => {
         console.log(data);
-        this.setState({ userData: data.Customers, header: data.Headers, origData: data.Customers, loading: true })
+        this.setState({
+          userData: data.Customers,
+          header: data.Headers,
+          origData: data.Customers,
+          loading: true,
+        });
       })
-      .catch(console.log)
-  }
+      .catch(console.log);
+  };
   render = () => {
     console.log(this.state.userData);
     console.log(this.state.header);
     return (
-
       <div className="App">
         <Suspense fallback={<h1>Loading data...</h1>}>
-          <CustomTable rows={this.state.userData} loading={this.state.loading} menus={this.state.menus} header={this.state.header} />
+          <CustomTable
+            rows={this.state.userData}
+            loading={this.state.loading}
+            menus={this.state.menus}
+            header={this.state.header}
+          />
         </Suspense>
-        
       </div>
-    )
-  }
-  
+    );
+  };
 }
 
 export default App;
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 ./gradlew customer-portal:clean customer-portal:build customer-portal:docker -x test
+./gradlew react:clean react:buildDockerImage
 docker-compose up -d
 rabbitmq_cont_id=$(docker ps -aqf "name=rabbitmq") && docker exec $rabbitmq_cont_id rabbitmq-plugins enable rabbitmq_management
